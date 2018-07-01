77fd9967c0c31273adb1ab1e87a8ef4ce400356a
Moniv9 <mohitverma3222@gmail.com>
2018-07-01 13:48:32 +0530

    adding min heap

--- min_heap.js
+++ min_heap.js
@@ -5,7 +5,7 @@ class MinHeap {
     }
 
     getParentIndex(index) {
-        return (index - 1) / 2;
+        return Math.floor((index - 1) / 2);
     }
 
     getLeftIndex(index) {
@@ -21,16 +21,64 @@ class MinHeap {
     }
 
     insertElement(value) {
-       
+        if (this.heapSize >= this.heap.length) {
+            this.heap.push(value);
+
+            let lastIndex = this.heap.length - 1;
+            let parentIndex = this.getParentIndex(lastIndex);
+
+            while (this.heap[parentIndex] > this.heap[lastIndex]) {
+                this._swap(parentIndex, lastIndex);
+
+                lastIndex = parentIndex;
+                parentIndex = this.getParentIndex(parentIndex);
+            }
+        } else {
+            console.warn('heap size limit reached');
+        }
     }
 
     removeRoot() {
+        if (this.heap.length === 0) {
+            console.warn('heap does not contain any element');
+        }
 
+        const lastIndex = this.heap.length - 1;
+        this.heap[0] = this.heap[lastIndex];
+        this.heap.splice(lastIndex, 1);
+
+        this._heapify(0);
     }
 
-    _heapify() {
+    _hasValue(value) {
+        return value !== undefined;
+    }
 
+    _swap(indexOne, indexTwo) {
+        const temp = this.heap[indexOne];
+        this.heap[indexOne] = this.heap[indexTwo];
+        this.heap[indexTwo] = temp;
+    }
+
+    _heapify(index) {
+        const leftIndex = this.getLeftIndex(index);
+        const rightIndex = this.getRightIndex(index);
+
+        if (index > this.heap.length) {
+            return;
+        }
+
+        if (this._hasValue(this.heap[leftIndex]) &&
+            this._hasValue(this.heap[rightIndex]) &&
+            this.heap[leftIndex] < this.heap[rightIndex]) {
+            this._swap(index, leftIndex);
+            this._heapify(leftIndex);
+        } else if (this._hasValue(this.heap[rightIndex]) && this.heap[rightIndex] < this.heap[index]) {
+            this._swap(index, rightIndex);
+            this._heapify(rightIndex);
+        } else if (this._hasValue(this.heap[leftIndex]) && this.heap[leftIndex] < this.heap[index]) {
+            this._swap(index, leftIndex);
+            this._heapify(leftIndex);
+        }
     }
 }
-
-const minHeap = new MinHeap(10);
