1fb5e691e244faf6f0bde55c4adef1345c321671
Moniv9 <mohitverma3222@gmail.com>
2018-07-15 14:52:02 +0530

    adding trie insert and search code

--- trie.js
+++ trie.js
@@ -1,5 +1,54 @@
-class Trie {
-    constructor() {
+class Node {
 
+    constructor() {
+        this.map = {};
     }
 }
+
+class Trie {
+
+    constructor() {
+        this.root = new Node();
+    }
+
+    insertNode(word) {
+        const length = word.length;
+        let currentNode = this.root;
+
+        for (let i = 0; i < length; i++) {
+            const ch = word[i];
+
+            if (currentNode && !currentNode.map[ch]) {
+                const newNode = new Node();
+                currentNode.map[ch] = newNode;
+                currentNode = newNode;
+            } else {
+                currentNode = currentNode.map[ch];
+            }
+        }
+    }
+
+    isWordAvailable(word) {
+        const length = word.length;
+        let currentNode = this.root;
+
+        for (let i = 0; i < length; i++) {
+            const ch = word[i];
+
+            if (currentNode && currentNode.map[ch]) {
+                currentNode = currentNode.map[ch];
+            } else {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+const trie = new Trie();
+trie.insertNode('hel');
+trie.insertNode('hl');
+
+console.log(trie.isWordAvailable('hey'));
+console.log(JSON.stringify(trie.root));
